3fc61267abab47cff19ddcbdcf9a6e77dc2e59e6
rayjo0620 <rayjo0620@gmail.com>
2020-12-29 11:14:53 +0900

    2020.12.29 rayjo0620 최초 커밋

---
+++ demoTest/src/main/java/com/example/demo/DemoTestApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestApplication.class, args);
+		
+		System.out.println("Hello world");
+	}
+
+}
